11776003ecef232b95555dcef6709612473e0ea2
Shawn Ren <shawnren527@gmail.com>
2019-04-08 19:31:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
 
   attr_reader :id, :name, :type, :db
 
-  def initialize(id, name, type, db)
+  def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
